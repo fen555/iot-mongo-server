1497bdbdb9f296171a82140ef091af6c63cc7859
CoFen <cofen.yensira@gmail.com>
2015-10-30 12:45:06 +0700

    Root Post

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 var homework = require('./models/homework/homework.route.js')
-app.use('/', homework)
+app.use('/api/homework', homework)
 
 
 var server = app.listen(3000, function () {
