e800dbbafbd8f842f28f8c43caa51ed2ef92bf87
CoFen <cofen.yensira@gmail.com>
2015-11-02 01:03:39 +0700

    change DB but can't insert in root but can insert in api/iot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var Model = require('./models/homework/homework.schema.js')
 
 mongoose.connect('mongodb://localhost:27017/iot_db')
 
@@ -12,6 +13,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 var homework = require('./models/homework/homework.route.js')
 app.use('/api/iot', homework)
+
+
+ app.post('/', function (req, res, next) {
+    var obj = new Model(req.body)
+    obj.save(function (err, obj) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.send(obj)
+      }
+    })
+  })
+
 
 
 var server = app.listen(3000, function () {
