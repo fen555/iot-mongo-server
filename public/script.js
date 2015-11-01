592603c5ae391d1413de52f070509ca65f7a8a27
CoFen <cofen.yensira@gmail.com>
2015-11-02 01:57:58 +0700

    add simple chart

--- public/script.js
+++ public/script.js
@@ -49,4 +49,53 @@ angular.module('app', [])
         
 
     // }
+
+    vm.graph = function(){
+      $http.get('/api/iot')
+              .then(function success (response) {
+         
+                  var data = {
+                              labels: [],
+                              datasets: [
+                                  {
+                                      label: "temperature",
+                                      fillColor: "rgba(255,0,0,0.2)",
+                                      strokeColor: "rgba(255,0,0,1)",
+                                      pointColor: "rgba(255,0,0,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(220,220,220,1)",
+                                      data: []
+                                  },
+                                  {
+                                      label: "relative_humidity",
+                                      fillColor: "rgba(69,187,91,0.2)",
+                                      strokeColor: "rgba(69,187,91,1)",
+                                      pointColor: "rgba(69,187,91,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(151,187,205,1)",
+                                      data: []
+                                  }
+                              ]
+                          };
+
+               var ctx = document.getElementById("c").getContext("2d")
+               var myLineChart = new Chart(ctx).Line(data);
+
+               
+                  for(var i =0;i<response.data.length;i++){
+                    if (response.data[i].iot_id==0){
+                         myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,"IOT_ID : 0");
+                       }
+                   
+                }
+               
+
+              }, function error (response) {
+                alert(response.data.message)
+              }) 
+      
+    }
+
   })
