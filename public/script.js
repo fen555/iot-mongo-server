a1df265be60ed081e784f010097001276f69052d
CoFen <cofen.yensira@gmail.com>
2015-11-04 06:51:06 +0700

    morn

--- public/script.js
+++ public/script.js
@@ -81,7 +81,7 @@ angular.module('app', [])
 
 
 
-    vm.graph = function(){
+    vm.graph0 = function(){
       $http.get('/api/iot')
               .then(function success (response) {
          
@@ -119,9 +119,104 @@ angular.module('app', [])
                     if (response.data[i].iot_id==0){
                          myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,vm.toThaiDateTime(response.data[i].timestamp));
                        }
-                    // if (response.data[i].iot_id==1){
-                    //      myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,vm.toThaiDateTime(response.data[i].timestamp));
-                    //    }
+                   
+                }
+               
+
+              }, function error (response) {
+                alert(response.data.message)
+              }) 
+      
+    }
+
+    vm.graph1 = function(){
+      $http.get('/api/iot')
+              .then(function success (response) {
+         
+                  var data = {
+                              labels: [],
+                              datasets: [
+                                  {
+                                      label: "temperature",
+                                      fillColor: "rgba(255,0,0,0.2)",
+                                      strokeColor: "rgba(255,0,0,1)",
+                                      pointColor: "rgba(255,0,0,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(220,220,220,1)",
+                                      data: []
+                                  },
+                                  {
+                                      label: "relative_humidity",
+                                      fillColor: "rgba(69,187,91,0.2)",
+                                      strokeColor: "rgba(69,187,91,1)",
+                                      pointColor: "rgba(69,187,91,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(151,187,205,1)",
+                                      data: []
+                                  }
+                              ]
+                          };
+
+               var ctx = document.getElementById("cc").getContext("2d")
+               var myLineChart = new Chart(ctx).Line(data);
+
+               
+                  for(var i =0;i<response.data.length;i++){
+                    if (response.data[i].iot_id==1){
+                         myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,vm.toThaiDateTime(response.data[i].timestamp));
+                       }
+                    
+                   
+                }
+               
+
+              }, function error (response) {
+                alert(response.data.message)
+              }) 
+      
+    }
+
+    vm.graph2 = function(){
+      $http.get('/api/iot')
+              .then(function success (response) {
+         
+                  var data = {
+                              labels: [],
+                              datasets: [
+                                  {
+                                      label: "temperature",
+                                      fillColor: "rgba(255,0,0,0.2)",
+                                      strokeColor: "rgba(255,0,0,1)",
+                                      pointColor: "rgba(255,0,0,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(220,220,220,1)",
+                                      data: []
+                                  },
+                                  {
+                                      label: "relative_humidity",
+                                      fillColor: "rgba(69,187,91,0.2)",
+                                      strokeColor: "rgba(69,187,91,1)",
+                                      pointColor: "rgba(69,187,91,1)",
+                                      pointStrokeColor: "#fff",
+                                      pointHighlightFill: "#fff",
+                                      pointHighlightStroke: "rgba(151,187,205,1)",
+                                      data: []
+                                  }
+                              ]
+                          };
+
+               var ctx = document.getElementById("ccc").getContext("2d")
+               var myLineChart = new Chart(ctx).Line(data);
+
+               
+                  for(var i =0;i<response.data.length;i++){
+                    if (response.data[i].iot_id==1){
+                         myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,vm.toThaiDateTime(response.data[i].timestamp));
+                       }
+                    
                    
                 }
                
